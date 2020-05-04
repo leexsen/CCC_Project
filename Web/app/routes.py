4c061d963a2706927da1220d4e30d9715e632768
Zhou Xuan <xuan.zhou@student.unimelb.edu.au>
2020-05-04 13:29:01 +1000

    learning flask & add a env for flaskapp setting

--- Web/app/routes.py
+++ Web/app/routes.py
@@ -1,6 +1,11 @@
 from app import app
 
+from flask import Flask, render_template
+
 @app.route('/')
+def homepage():
+    return render_template("homepage.html")
+
 @app.route('/index')
 def index():
     return "Hello, World!"
