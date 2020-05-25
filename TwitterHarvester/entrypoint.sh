506b377a1d4c9935397669495909f10a094ce1e7
Sean Li <leexsen@gmail.com>
2020-05-25 16:49:39 +1000

    Ansible: added tasks for creating the web service

--- TwitterHarvester/entrypoint.sh
+++ TwitterHarvester/entrypoint.sh
@@ -3,8 +3,6 @@
 # turn on bash's job control
 set -m
   
-# Start the primary process and put it in the background
 python stream_harvester.py &
-  
-# Start the helper process
-python history_harvester.py
+python history_harvester.py &
+python user_harvester.py
